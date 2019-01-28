f1d8aaf72ec7f119f7b640940d05618c23b5b5d8
aklsh <akileshkannan@gmail.com>
2019-01-28 22:20:07 +0530

    Updated code to get .vcd file

--- Digital Systems - Lab/Experiment 2/testbench_5bit.v
+++ Digital Systems - Lab/Experiment 2/testbench_5bit.v
@@ -1,6 +1,11 @@
 `timescale 1ns/100ps
 
 module test_5bit_adder;
+	
+	initial begin
+		$dumpfile("five_bit_adder.vcd");
+		$dumpvars(0, test_5bit_adder); //Name of testbench module
+	end
 
 	wire[4:0] sum;
 	wire cout;
@@ -14,7 +19,9 @@ module test_5bit_adder;
 	begin
 	$timeformat(-9,1,"ns",5);
 	$monitor("At t = %t, a = %5b, b = %5b, sum = %5b, cout = %b", $time, i1, i2, sum, cout);
-	i1 = 5'b10011; i2 = 5'b10010; cin=1'b0;
+	i1 = 5'b10101; i2 = 5'b10010; cin=1'b0;
+
 	#10 $finish;
 	end
+
 endmodule
