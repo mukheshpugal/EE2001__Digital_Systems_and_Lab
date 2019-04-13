9b14da4677691ee832b04023de88aaf52d9c5c9b
Akilesh Kannan <45280368+aklsh@users.noreply.github.com>
2019-04-13 21:32:15 +0530

    clkdiv.v (our version) with test bench
    Our version, which didnt work in lab, now works. dk what error made in lab. same code works here, doesn't work there

--- Digital Systems - Lab/Experiment 11/clk1h.v
+++ Digital Systems - Lab/Experiment 11/clk1h.v
@@ -1,44 +1,63 @@
-`timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer: Akilesh Kannan
 // 
 // Create Date:    15:56:38 04/02/2019 
 // Design Name: 
 // Module Name:    clk1h 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
+// Project Name: Experiment 11
+// Description: Divides a 1 Mhz clock into 1Hz clock
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1us / 100ns
+
 module clk1h(input clk, input rst, output clk1hz);
 	 reg clk1hnew;
-	 reg[24:0] counter;
+	reg[25:0] counter;
 	 
 	 initial begin 
 		counter = 0;
 		clk1hnew = 0;
 	 end
 	 
-	 always @(posedge clk)
+	always @(posedge clk or negedge clk)
 	 begin
-		if(!rst==0)
+		if(rst)
 			clk1hnew <= 0;
-		else if (counter==0)
+		 else if (counter==1000000)
 			 begin
-				counter <= 24999999;
+				counter <= 0;
 				clk1hnew <= ~clk1hz;
 			 end
 		else
-			counter <= counter - 1;
+			counter <= counter + 1;
 	 end	
 	 assign clk1hz = clk1hnew;
 endmodule
 
+module test_bench;
+
+	initial 
+	begin
+		$dumpfile("clk1h.vcd");
+		$dumpvars(0,test_bench);
+	end
+	
+	reg clk, rst;
+	wire clk1hz;
+
+	clk1h c(clk, rst, clk1hz);
+
+	initial
+	begin
+		rst=0;
+		clk=0;
+		forever #1 clk = ~clk;
+	end
+
+	initial
+	begin
+		#1000000 rst=1'b1;
+		#1000000 rst=1'b0;
+		#4000000 $finish;
+	end
+endmodule
